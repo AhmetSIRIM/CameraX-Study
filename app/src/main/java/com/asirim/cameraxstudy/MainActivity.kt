128f0f577fc13d80b6be6768a072f50267033ef0
Ahmet SIRIM <ahmet.sirim@outlook.com>
2023-08-31 18:27:51 +0300

    Combine imageCapture and imageAnalyzer at the end as my test device does not support imageCapture and videoCapture together

--- app/src/main/java/com/asirim/cameraxstudy/MainActivity.kt
+++ app/src/main/java/com/asirim/cameraxstudy/MainActivity.kt
@@ -17,16 +17,7 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.video.FallbackStrategy
-import androidx.camera.video.MediaStoreOutputOptions
-import androidx.camera.video.Quality
-import androidx.camera.video.QualitySelector
-import androidx.camera.video.Recorder
-import androidx.camera.video.Recording
-import androidx.camera.video.VideoCapture
-import androidx.camera.video.VideoRecordEvent
 import androidx.core.content.ContextCompat
-import androidx.core.content.PermissionChecker
 import com.asirim.cameraxstudy.databinding.ActivityMainBinding
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
@@ -34,6 +25,11 @@ import java.util.Locale
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+/**
+ * After 'Step 8' the following log wrote. After this log, I changed something at 'My Personal Step or Last Step'.
+ *
+ * No supported surface combination is found for camera device - Id : 1.  May be attempting to bind too many use cases. Existing surfaces: [] New configs: [androidx.camera.video.impl.VideoCaptureConfig@a0ec75c, androidx.camera.core.impl.ImageCaptureConfig@ec9c1cf, androidx.camera.core.impl.PreviewConfig@791222e]
+ */
 typealias LumaListener = (luma: Double) -> Unit
 
 class MainActivity : AppCompatActivity() {
@@ -43,9 +39,14 @@ class MainActivity : AppCompatActivity() {
     // Image operation
     private var imageCapture: ImageCapture? = null
 
+    /**
+     * My Personal Step or Last Step
+     *
+     * 'videoCapture' and 'recording' closed at 'Combine imageCapture and imageAnalyzer at the end' commit
+     * */
     // Video operation
-    private var videoCapture: VideoCapture<Recorder>? = null
-    private var recording: Recording? = null
+//    private var videoCapture: VideoCapture<Recorder>? = null
+//    private var recording: Recording? = null
 
     private lateinit var cameraExecutor: ExecutorService
 
@@ -61,7 +62,12 @@ class MainActivity : AppCompatActivity() {
 
         // Set up the listeners for take photo and video capture buttons
         binding.buttonImageCapture.setOnClickListener { takePhoto() }
-        binding.buttonVideoCapture.setOnClickListener { captureVideo() }
+        /**
+         * My Personal Step or Last Step
+         *
+         * 'buttonVideoCapture.setOnClickListener' closed at 'Combine imageCapture and imageAnalyzer at the end' commit
+         * */
+//        binding.buttonVideoCapture.setOnClickListener { captureVideo() }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
 
@@ -136,6 +142,9 @@ class MainActivity : AppCompatActivity() {
      *
      * Step 7.2
      * * Edited at 'Implement VideoCapture use case' commit
+     *
+     * My Personal Step or Last Step
+     * * Edited at 'Combine imageCapture and imageAnalyzer at the end' commit
      * */
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
@@ -153,30 +162,31 @@ class MainActivity : AppCompatActivity() {
                     )
                 }
 
-            /**
-             * Step 8
-             *
-             * 'imageCapture' opened at 'Combine VideoCapture with other use cases' commit
-             * */
             imageCapture = ImageCapture.Builder()
                 .build()
 
-//            val imageAnalyzer = ImageAnalysis.Builder()
-//                .build()
-//                .also {
-//                    it.setAnalyzer(
-//                        cameraExecutor,
-//                        LuminosityAnalyzer { luma ->
-//                            Log.d(TAG, "Average luminosity: $luma")
-//                        }
-//                    )
-//                }
-
-            val recorder = Recorder.Builder()
-                .setQualitySelector(QualitySelector.from(Quality.HIGHEST,
-                    FallbackStrategy.higherQualityOrLowerThan(Quality.SD)))
+            val imageAnalyzer = ImageAnalysis.Builder()
                 .build()
-            videoCapture = VideoCapture.withOutput(recorder)
+                .also {
+                    it.setAnalyzer(
+                        cameraExecutor,
+                        LuminosityAnalyzer { luma ->
+                            Log.d(TAG, "Average luminosity: $luma")
+                        }
+                    )
+                }
+
+
+            /**
+             * My Personal Step or Last Step
+             *
+             * 'recorder' and 'videoCapture' closed at 'Combine imageCapture and imageAnalyzer at the end' commit
+             * */
+//            val recorder = Recorder.Builder()
+//                .setQualitySelector(QualitySelector.from(Quality.HIGHEST,
+//                    FallbackStrategy.higherQualityOrLowerThan(Quality.SD)))
+//                .build()
+//            videoCapture = VideoCapture.withOutput(recorder)
 
             // Select a camera as a default
             val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
@@ -190,14 +200,14 @@ class MainActivity : AppCompatActivity() {
                     this,
                     cameraSelector,
                     preview,
-                    /**
-                     * Step 8
-                     *
-                     * 'imageCapture' opened at 'Combine VideoCapture with other use cases' commit
-                     * */
                     imageCapture,
-//                    imageAnalyzer,
-                    videoCapture
+                    imageAnalyzer,
+                    /**
+                     * My Personal Step or Last Step
+                     *
+                     * 'videoCapture' closed at 'Combine imageCapture and imageAnalyzer at the end' commit
+                     * */
+//                    videoCapture
                 )
 
             } catch (exc: Exception) {
@@ -290,85 +300,85 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Step 7.1
+     * My Personal Step or Last Step
      *
-     * Created at 'Implement VideoCapture use case' commit
+     * 'captureVideo' closed at 'Combine imageCapture and imageAnalyzer at the end' commit
      * */
     // Implements VideoCapture use case, including start and stop capturing.
-    private fun captureVideo() {
-        val videoCapture = this.videoCapture ?: return
-
-        binding.buttonVideoCapture.isEnabled = false
-
-        val currentRecording = recording
-        if (currentRecording != null) {
-            // Stop the current recording session.
-            currentRecording.stop()
-            recording = null
-            return
-        }
-
-        // create and start a new recording session
-        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
-            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
-            }
-        }
-
-        val mediaStoreOutputOptions = MediaStoreOutputOptions
-            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
-            .setContentValues(contentValues)
-            .build()
-        recording = videoCapture.output
-            .prepareRecording(this, mediaStoreOutputOptions)
-            .apply {
-                if (PermissionChecker.checkSelfPermission(
-                        this@MainActivity,
-                        Manifest.permission.RECORD_AUDIO
-                    ) ==
-                    PermissionChecker.PERMISSION_GRANTED
-                ) {
-                    withAudioEnabled()
-                }
-            }.start(ContextCompat.getMainExecutor(this)) { recordEvent ->
-                when (recordEvent) {
-                    is VideoRecordEvent.Start -> {
-                        binding.buttonVideoCapture.apply {
-                            text = getString(R.string.stop_capture)
-                            isEnabled = true
-                        }
-                    }
-
-                    is VideoRecordEvent.Finalize -> {
-                        if (!recordEvent.hasError()) {
-                            val message = "Video capture succeeded: ${
-                                recordEvent.outputResults.outputUri
-                            }"
-                            Toast.makeText(baseContext, message, Toast.LENGTH_SHORT)
-                                .show()
-                            Log.d(TAG, message)
-                        } else {
-                            recording?.close()
-                            recording = null
-                            Log.e(
-                                TAG,
-                                "Video capture ends with error: ${
-                                    recordEvent.error
-                                }"
-                            )
-                        }
-                        binding.buttonVideoCapture.apply {
-                            text = getString(R.string.start_capture)
-                            isEnabled = true
-                        }
-                    }
-                }
-            }
-    }
+//    private fun captureVideo() {
+//        val videoCapture = this.videoCapture ?: return
+//
+//        binding.buttonVideoCapture.isEnabled = false
+//
+//        val currentRecording = recording
+//        if (currentRecording != null) {
+//            // Stop the current recording session.
+//            currentRecording.stop()
+//            recording = null
+//            return
+//        }
+//
+//        // create and start a new recording session
+//        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+//            .format(System.currentTimeMillis())
+//        val contentValues = ContentValues().apply {
+//            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+//            put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
+//            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+//                put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
+//            }
+//        }
+//
+//        val mediaStoreOutputOptions = MediaStoreOutputOptions
+//            .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+//            .setContentValues(contentValues)
+//            .build()
+//        recording = videoCapture.output
+//            .prepareRecording(this, mediaStoreOutputOptions)
+//            .apply {
+//                if (PermissionChecker.checkSelfPermission(
+//                        this@MainActivity,
+//                        Manifest.permission.RECORD_AUDIO
+//                    ) ==
+//                    PermissionChecker.PERMISSION_GRANTED
+//                ) {
+//                    withAudioEnabled()
+//                }
+//            }.start(ContextCompat.getMainExecutor(this)) { recordEvent ->
+//                when (recordEvent) {
+//                    is VideoRecordEvent.Start -> {
+//                        binding.buttonVideoCapture.apply {
+//                            text = getString(R.string.stop_capture)
+//                            isEnabled = true
+//                        }
+//                    }
+//
+//                    is VideoRecordEvent.Finalize -> {
+//                        if (!recordEvent.hasError()) {
+//                            val message = "Video capture succeeded: ${
+//                                recordEvent.outputResults.outputUri
+//                            }"
+//                            Toast.makeText(baseContext, message, Toast.LENGTH_SHORT)
+//                                .show()
+//                            Log.d(TAG, message)
+//                        } else {
+//                            recording?.close()
+//                            recording = null
+//                            Log.e(
+//                                TAG,
+//                                "Video capture ends with error: ${
+//                                    recordEvent.error
+//                                }"
+//                            )
+//                        }
+//                        binding.buttonVideoCapture.apply {
+//                            text = getString(R.string.start_capture)
+//                            isEnabled = true
+//                        }
+//                    }
+//                }
+//            }
+//    }
 
     companion object {
         private const val TAG = "CameraX Study"
